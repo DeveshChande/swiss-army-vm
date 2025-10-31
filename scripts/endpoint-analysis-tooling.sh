2056f60cfc3db564f3831945343937519061dd58
v <chandedevesh@proton.me>
2025-10-31 10:08:04 -0400

    Added evtx in endpoint-analysis-tooling.sh

--- scripts/endpoint-analysis-tooling.sh
+++ scripts/endpoint-analysis-tooling.sh
@@ -5,3 +5,7 @@ sudo wget "https://github.com/Velocidex/velociraptor/releases/download/v0.75/vel
 
 # OSQuery Installation
 wget "https://pkg.osquery.io/deb/osquery_5.19.0-1.linux_amd64.deb" && apt install ./osquery_5.19.0-1.linux_amd64.deb
+
+# evtx
+sudo curl -fL -o /usr/bin/evtx-dump https://github.com/omerbenamram/evtx/releases/download/v0.9.0/evtx_dump-v0.9.0-x86_64-unknown-linux-gnu
+sudo chmod +x /usr/bin/evtx-dump
