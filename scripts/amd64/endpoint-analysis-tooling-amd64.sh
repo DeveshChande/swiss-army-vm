3c7dc0cbd520e56b8619396a9642acb734dbd01d
v <chandedevesh@proton.me>
2025-11-01 20:47:34 -0400

    Added file-analysis-tooling-amd64.sh

--- scripts/amd64/endpoint-analysis-tooling-amd64.sh
+++ scripts/amd64/endpoint-analysis-tooling-amd64.sh
@@ -10,14 +10,3 @@ wget "https://pkg.osquery.io/deb/osquery_5.19.0-1.linux_amd64.deb" && apt instal
 sudo curl -fL -o /usr/bin/evtx-dump https://github.com/omerbenamram/evtx/releases/download/v0.9.0/evtx_dump-v0.9.0-x86_64-unknown-linux-gnu
 sudo chmod +x /usr/bin/evtx-dump
 
-# YARA
-apt -y install automake libtool make gcc pkg-config
-cd /home/packer/Downloads
-sudo curl -fLO https://github.com/VirusTotal/yara/archive/refs/tags/v4.5.5.tar.gz
-sudo tar -xzf v4.5.5.tar.gz
-cd /home/packer/Downloads/yara-4.5.5
-sudo ./bootstrap.sh
-sudo ./configure
-sudo make
-sudo make install
-sudo cp /home/packer/Downloads/yara-4.5.5/yara /usr/bin/
---
+++ scripts/amd64/file-analysis-tooling.sh
@@ -0,0 +1,26 @@
+# binwalk
+cd /home/packer
+ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+. $HOME/.cargo/env
+cd /home/packer/Downloads
+git clone https://github.com/ReFirmLabs/binwalk
+apt -y install build-essential libfontconfig1-dev liblzma-dev
+cd binwalk
+cargo build --release
+cp /home/packer/Downloads/binwalk/target/release/binwalk /usr/bin/binwalk
+
+# hashdeep
+apt -y install hashdeep
+
+# YARA
+apt -y install automake libtool make gcc pkg-config
+cd /home/packer/Downloads
+sudo curl -fLO https://github.com/VirusTotal/yara/archive/refs/tags/v4.5.5.tar.gz
+sudo tar -xzf v4.5.5.tar.gz
+cd /home/packer/Downloads/yara-4.5.5
+sudo ./bootstrap.sh
+sudo ./configure
+sudo make
+sudo make install
+cp /home/packer/Downloads/yara-4.5.5/yara /usr/bin
+
