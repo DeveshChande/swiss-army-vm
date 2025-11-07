52331e36c4adf6cdfd41de27cb8ed33dc50770f9
Mohammad Abraar <53563942+Abraar02@users.noreply.github.com>
2025-11-07 14:55:20 -0500

    Add installation of essential packages in script

--- arm64/scripts/system-initial-configuration-arm64.sh
+++ arm64/scripts/system-initial-configuration-arm64.sh
@@ -1,2 +1,7 @@
 apt update && apt -y upgrade
 sudo -u packer env XDG_RUNTIME_DIR=/run/user/$(id -u packer) xdg-user-dirs-update
+
+apt -y install curl wget vim
+apt -y install python3 python3-pip
+apt -y install git build-essential cmake libpcap-dev libboost-all-dev
+apt -y install python3-magic
