e1c781c8dc25b7867aa2b656f38ba5252dd28606
Mohammad Abraar <53563942+Abraar02@users.noreply.github.com>
2025-11-07 14:47:58 -0500

    Added endpoint-analysis-tooling script
    Added installation steps for Log2timeline and Dumpzilla.

--- scripts/arm64/endpoint-analysis-tooling-arm64.sh
+++ scripts/arm64/endpoint-analysis-tooling-arm64.sh
@@ -24,3 +24,15 @@ if [ -f "./volatility3/requirements.txt" ]; then
 else
 ./vol3-venv/bin/pip install ./volatility3
 fi
+
+# Log2timeline Installation
+sudo pip3 install --break-system-packages plaso
+
+# Create python symlink for compatibility
+sudo ln -sf /usr/bin/python3 /usr/bin/python
+
+# Dumpzilla Installation
+cd /opt
+sudo git clone https://github.com/Busindre/dumpzilla.git
+sudo chmod +x /opt/dumpzilla/dumpzilla.py
+sudo ln -sf /opt/dumpzilla/dumpzilla.py /usr/local/bin/dumpzilla
